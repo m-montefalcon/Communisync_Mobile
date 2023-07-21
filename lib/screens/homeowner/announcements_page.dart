fb91a77de7b8cb6efb4e4b4f9d78e5de790e160c
Eljohn Bert Ronolo <eljohnbertron@gmail.com>
2023-07-21 15:31:27 +0800

    announcemet page update

--- lib/screens/homeowner/announcements_page.dart
+++ lib/screens/homeowner/announcements_page.dart
@@ -49,18 +49,263 @@ class _AnnouncementPageState extends State<AnnouncementPage> {
             ),
           ),
         ],
-        body: Column(
-          children: const [
-            SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: Text(
-                'Announcements',
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  Row(
+
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Announcements',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 12,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        color: Colors.purple,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 20.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(colors: [
+                              Colors.purple.shade800,
+                              Colors.purple.shade400
+                            ]),
+                          ),
+                          child: Stack(
+                            children: [
+                              const Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Text('6/15/2023',
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    const Text(
+                                      'Implementation of CommuniSync app',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: const [
+                                        Text(
+                                          'Details:',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                            width:
+                                            5), // Add some spacing between the texts
+                                      ],
+                                    ),
+                                    const Text(
+                                      'The implementation of the Communisync app'
+                                      ' involves several key steps to ensure its '
+                                      'successful development and deployment. '
+                                      'Firstly, a team of skilled developers and '
+                                      'designers collaborate to create a user-friendly '
+                                      'interface and design the apps features.',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 35),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 12,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        color: Colors.purple,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 20.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(colors: [
+                              Colors.purple.shade800,
+                              Colors.purple.shade400
+                            ]),
+                          ),
+                          child: Stack(
+                            children: [
+                              const Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Text('7/21/2023',
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    const Text(
+                                      'Libreng tuli sa basketball court!',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: const [
+                                        Text(
+                                          'Details:',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                            width:
+                                            5), // Add some spacing between the texts
+                                      ],
+                                    ),
+                                    const Text(
+                                      'Maayong adlaw sa tanan, giawhag ang tanan'
+                                          ' mga pisot dari sa Greenville Subdivision'
+                                          ' barangay Bugo ning syudad sa dakbayan sa'
+                                          ' Cagayan de Oro na magpatuli na kay naay'
+                                          ' gipahigayon nga libreng tuli gipangulohan'
+                                          ' ni Kapitan Meinardz Montefalcon uban'
+                                          ' sa iyang mga konseho.',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 35),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 12,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        color: Colors.purple,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 20.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(colors: [
+                              Colors.purple.shade800,
+                              Colors.purple.shade400
+                            ]),
+                          ),
+                          child: Stack(
+                            children: [
+                              const Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Text('8/19/2023',
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    const Text(
+                                      'No QR Code no entry policy',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: const [
+                                        Text(
+                                          'Details:',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                            width:
+                                            5), // Add some spacing between the texts
+                                      ],
+                                    ),
+                                    const Text(
+                                      'The implementation of the Communisync app'
+                                          ' involves several key steps to ensure its '
+                                          'successful development and deployment. '
+                                          'Firstly, a team of skilled developers and '
+                                          'designers collaborate to create a user-friendly '
+                                          'interface and design the apps features.',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 35),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
