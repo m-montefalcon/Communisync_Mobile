14eb48863dd0317652ff84924631f2f5520b5874
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-09-21 23:47:39 +0800

    register functinality added

--- lib/backend/api/auth/register_auth.dart
+++ lib/backend/api/auth/register_auth.dart
@@ -1,42 +1,53 @@
 import 'dart:convert';
-
+import 'package:communisyncmobile/screens/login_page.dart';
 import 'package:communisyncmobile/screens/visitor/visitor_bttmbar.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future <void> registerUser(context, String userName, String firstName, String lastName,
-    String contactNumber, String email, String password)async {
-    try{
-      String host = dotenv.get("API_HOST", fallback: "");
-      String registerAPi = dotenv.get("REGISTER_API", fallback: "");
-      final url = ('$host$registerAPi');
-      final response = await http.post(
-        Uri.parse(url),
-        body: {
-          'user_name' : userName,
-          'email' : email,
-          'first_name' : firstName,
-          'last_name' : lastName,
-          'contact_number' : contactNumber,
-          'password' : password,
-          'role' : 1
-        }
-      );
+Future<void> registerUser(
+    context,
+    String userName,
+    String firstName,
+    String lastName,
+    String contactNumber,
+    String email,
+    String password,
+    ) async {
+  try {
+    String host = dotenv.get("API_HOST", fallback: "");
+    String registerApi = dotenv.get("REGISTER_API", fallback: "");
+    final url = ('$host$registerApi');
+    print("URL: $url");
+    final response = await http.post(
+      Uri.parse(url),
+      body: {
+        'user_name': userName,
+        'email': email,
+        'first_name': firstName,
+        'last_name': lastName,
+        'contact_number': contactNumber,
+        'password': password,
+      },
+      headers: {'Accept': 'application/json'},
+    );
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
 
-      if(response.statusCode == 200){
-        print('Login successful');
-        Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => const VisitorBottombar()),
-              (route) => false,
-        );
-      }
-      else{
-        throw Exception('Invalid credentials');
-      }
+    if (response.statusCode == 200) {
+      print('Login successful');
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+            (route) => false,
+      );
+    } else {
+      final responseBody = jsonDecode(response.body);
+      final errorMessage = responseBody['message'];
+      throw (errorMessage);
     }
-    catch(e){
-      throw Exception('Invalid');
-    }
+  } catch (e) {
+    print("Exception caught in registerUser: ");
+    throw (e);
+  }
 }
