b5f67af1ba2d95a676e4a7021ea1c73d6a0679cf
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-09-23 15:41:43 +0800

    changes on request caf on dashboard and new pages for all caf requests

--- lib/backend/model/request_class.dart
+++ lib/backend/model/request_class.dart
@@ -4,11 +4,11 @@ class Request {
   final int homeownerId;
   final int? adminId;
   final int? personnelId;
-  // final DateTime date;
-  // final DateTime time;
-  // final String destinationPerson;
-  // final List<String>? visitMembers;
-  // final  visitStatus;
+  final DateTime date;
+  final DateTime time;
+  final String destinationPerson;
+  final List<String>? visitMembers;
+  final String visitStatus;
   // final String? qrCode;
   // final DateTime createdAt;
   // final DateTime updatedAt;
@@ -20,14 +20,12 @@ class Request {
     required this.homeownerId,
     this.adminId,
     this.personnelId,
-    // required this.date,
-    // required this.time,
-    // required this.destinationPerson,
-    // required this.visitMembers,
-    // required this.visitStatus,
-    // this.qrCode,
-    // required this.createdAt,
-    // required this.updatedAt,
+    required this.date,
+    required this.time,
+    required this.destinationPerson,
+    required this.visitMembers,
+    required this.visitStatus,
+
     required this.visitor,
   });
 }
@@ -35,36 +33,22 @@ class Request {
 
 class Visitor {
   final int id;
-  // final String userName;
-  // final String firstName;
-  // final String lastName;
-  // final String contactNumber;
-  // final String? blockNo;
-  // final String? lotNo;
-  // final String? familyMember;
-  // final String? emailVerifiedAt;
-  // final int? manualVisitOption;
-  // final String? photo;
-  // final int role;
-  // final String email;
-  // final DateTime createdAt;
-  // final DateTime updatedAt;
-
+  final String userName;
+  final String firstName;
+  final String lastName;
+  final String contactNumber;
+  final String? photo;
+  final String role;
+  final String email;
   Visitor({
     required this.id,
-    // required this.userName,
-    // required this.firstName,
-    // required this.lastName,
-    // required this.contactNumber,
-    // this.blockNo,
-    // this.lotNo,
-    // this.familyMember,
-    // this.emailVerifiedAt,
-    // this.manualVisitOption,
-    // this.photo,
-    // required this.role,
-    // required this.email,
-    // required this.createdAt,
-    // required this.updatedAt,
+    required this.userName,
+    required this.firstName,
+    required this.lastName,
+    required this.contactNumber,
+    this.photo,
+    required this.role,
+    required this.email,
+
   });
 }
