85ae382c41642370a412edeb317a6b15486f04cf
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-09-28 01:52:17 +0800

    fixes on visit request null handling

--- lib/screens/visitor/visitor_dashboard_page.dart
+++ lib/screens/visitor/visitor_dashboard_page.dart
@@ -1,8 +1,8 @@
-import 'package:communisyncmobile/backend/api/visitor/fetch_all_validated_request.dart';
 import 'package:communisyncmobile/backend/model/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-// Import your model class and fetchAllRequestApi function here.
+
+import '../../backend/api/visitor/fetch_all_validated_request.dart';
 
 class VisitorDashboardPage extends StatefulWidget {
   const VisitorDashboardPage({Key? key}) : super(key: key);
@@ -12,33 +12,11 @@ class VisitorDashboardPage extends StatefulWidget {
 }
 
 class _VisitorDashboardPageState extends State<VisitorDashboardPage> {
-  List<FetchAllQr> fetchAllData = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchDataFromApi();
-  }
-
-  Future<void> fetchDataFromApi() async {
-    try {
-      final data = await fetchAllRequestApi(context);
-
-      setState(() {
-        fetchAllData = data;
-      });
-    } catch (e) {
-      // Handle errors here.
-      print('Error fetching data: $e');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        floatHeaderSlivers: true,
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+      body: CustomScrollView(
+        slivers: <Widget>[
           SliverAppBar(
             floating: true,
             backgroundColor: Colors.transparent,
@@ -65,92 +43,84 @@ class _VisitorDashboardPageState extends State<VisitorDashboardPage> {
               ),
             ),
           ),
-        ],
-          body: FutureBuilder<List<FetchAllQr>>(
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Visitation Requests',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FutureBuilder<List<FetchAllQr>>(
             future: fetchAllRequestApi(context),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
+                return SliverFillRemaining(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
                 );
               } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('Error: ${snapshot.error}'),
+                return SliverFillRemaining(
+                  child: Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  ),
                 );
               } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Center(
-                  child: Text('No data available'),
+                return SliverFillRemaining(
+                  child: Center(
+                    child: Text('No data available'),
+                  ),
                 );
               } else {
                 final fetchAllData = snapshot.data; // Retrieve the fetched data
 
-                return SingleChildScrollView(
-                  child: SafeArea(
-                    child: Center(
-                      child: Column(
+                return SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                      final item = fetchAllData?[index];
+                      return Column(
                         children: [
-                          Card(
-                            elevation: 12,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24),
-                            ),
-                            color: Colors.purple,
-                          ),
-                          const SizedBox(height: 15),
-                          Row(
-                            children: const [
-                              Padding(
-                                padding: EdgeInsets.only(left: 15.0),
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    'Visitation Requests',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600,
-                                    ),
+                          ListTile(
+                            title: Text('ID: ${item?.id}'),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Destination Person: ${item?.destinationPerson ?? 'None'}'),
+                                Text('Homeowner: ${item?.homeowner != null ? '${item?.homeowner!.firstName ?? 'None'} ${item?.homeowner!.lastName ?? 'None'}' : 'None'}'),
+                                Text('Admin: ${item?.admin != null ? '${item?.admin!.firstName ?? 'None'} ${item?.admin!.lastName ?? 'None'}' : 'None'}'),
+                                Text('Visit Members: ${item?.visitMembers?.join(', ') ?? 'None'}'),
+                                SizedBox(
+                                  width: 200,
+                                  height: 200,
+                                  child: SvgPicture.string(
+                                    item?.qrCode ?? '', // Assuming item?.qrCode contains the SVG data
+                                    width: 200, // Set the desired width
+                                    height: 200, // Set the desired height
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-
-                          // Display the ListTiles with IDs.
-                          ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: fetchAllData?.length,
-                            itemBuilder: (context, index) {
-                              final item = fetchAllData?[index];
-                              return ListTile(
-                                title: Text('ID: ${item?.id}'),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text('Destination Person: ${item?.destinationPerson ?? 'None'}'),
-                                    Text('Homeowner: ${item?.homeowner != null ? '${item?.homeowner!.firstName ?? 'None'} ${item?.homeowner!.lastName ?? 'None'}' : 'None'}'),
-                                    Text('Admin: ${item?.admin != null ? '${item?.admin!.firstName ?? 'None'} ${item?.admin!.lastName ?? 'None'}' : 'None'}'),
-                                    Text('Visit Members: ${item?.visitMembers?.join(', ') ?? 'None'}'),
-                                    SvgPicture.string(
-                                      item?.qrCode ?? '',  // Assuming item?.qrCode contains the SVG data
-                                      width: 200,          // Set the desired width
-                                      height: 200,         // Set the desired height
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                          ),
-
+                          Divider(),
                         ],
-                      ),
-                    ),
+                      );
+                    },
+                    childCount: fetchAllData?.length ?? 0,
                   ),
                 );
               }
             },
-          )
-
+          ),
+        ],
       ),
     );
   }
