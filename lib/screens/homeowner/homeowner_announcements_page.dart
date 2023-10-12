69714616106ed952d12f220775b03fb8953bfacf
Eljohn Bert Ronolo <eljohnbertron@gmail.com>
2023-10-13 01:19:38 +0800

    announcement page update

--- lib/screens/homeowner/homeowner_announcements_page.dart
+++ lib/screens/homeowner/homeowner_announcements_page.dart
@@ -17,6 +17,15 @@ class _AnnouncementPageState extends State<AnnouncementPage> {
   Widget build(BuildContext context) {
     String host = dotenv.get("API_HOST", fallback: "");
 
+    String truncateDescription(String description) {
+      List<String> words = description.split(' ');
+      if (words.length > 4) {
+        return '${words.sublist(0, 4).join(' ')}...';
+      } else {
+        return description;
+      }
+    }
+
     return Scaffold(
       body: NestedScrollView(
         floatHeaderSlivers: true,
@@ -93,103 +102,114 @@ class _AnnouncementPageState extends State<AnnouncementPage> {
                 } else {
                   final List<Announcement> announcementData = snapshot.data!;
 
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: announcementData.length,
-                    itemBuilder: (context, index) {
-                      final Announcement data = announcementData[index];
-                      return Stack(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => SpecificAnnouncementPage(data: data),
-                                ),
-                              );
-                            },
-                            child: Card(
-                              margin: const EdgeInsets.all(10),
-                              elevation: 12,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24),
-                              ),
-                              color: Colors.purple,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-                                decoration: BoxDecoration(
+                  return Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: announcementData.length,
+                      itemBuilder: (context, index) {
+                        final Announcement data = announcementData[index];
+                        return Stack(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => SpecificAnnouncementPage(data: data),
+                                  ),
+                                );
+                              },
+                              child: Card(
+                                margin: const EdgeInsets.all(10),
+                                elevation: 12,
+                                shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(24),
-                                  gradient: LinearGradient(colors: [
-                                    Colors.purple.shade800,
-                                    Colors.purple.shade400
-                                  ]),
                                 ),
-                                child: Stack(
-                                  children: [
-                                    Positioned(
-                                      bottom: 0,
-                                      right: 0,
-                                      child: Text('${data.date}',
-                                          style: TextStyle(color: Colors.white)),
-                                    ),
-                                    Row(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        CircleAvatar(
-                                          radius: 24,
-                                          backgroundImage: NetworkImage('${host ?? ''}/storage/${data.admin.photo}'),
-                                        ),
-                                        SizedBox(width: 10),
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: <Widget>[
-                                              Text(
-                                                '${data.admin.firstName} ${data.admin.lastName}',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                              SizedBox(height: 10),
-                                              Text(
-                                                '${data.title}',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 15,
-                                                ),
-                                              ),
-                                              Text(
-                                                'Details:',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 15,
-                                                ),
-                                              ),
-                                              Text(
-                                                '${data.description}',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 15,
-                                                ),
-                                              ),
-                                              SizedBox(height: 35),
-                                            ],
+                                color: Colors.purple,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(24),
+                                    gradient: LinearGradient(colors: [
+                                      Colors.purple.shade800,
+                                      Colors.purple.shade400
+                                    ]),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        child: Text('Date: ${data.date}',
+                                            style: TextStyle(color: Colors.greenAccent)),
+                                      ),
+                                      Row(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          CircleAvatar(
+                                            radius: 24,
+                                            backgroundImage: NetworkImage('${host ?? ''}/storage/${data.admin.photo}'),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                          SizedBox(width: 10),
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                Text(
+                                                  '${data.admin.firstName} ${data.admin.lastName}',
+                                                  style: TextStyle(
+                                                    color: Colors.greenAccent,
+                                                    fontSize: 15,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  'Admin',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 15,
+                                                    fontWeight: FontWeight.normal,
+                                                  ),
+                                                ),
+                                                SizedBox(height: 7),
+                                                Text(
+                                                  'Title: ${data.title}',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 15,
+                                                  ),
+                                                ),
+                                                SizedBox(height: 7),
+                                                Text(
+                                                  'Details:',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 15,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  truncateDescription(data.description),
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 15,
+                                                  ),
+                                                ),
+                                                SizedBox(height: 35),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          )
+                            )
 
-                        ],
-                      );
-                    },
+                          ],
+                        );
+                      },
+                    ),
                   );
                 }
               },
