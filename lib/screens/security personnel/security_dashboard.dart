518c2fa35aecc76cb221a9ff377467f0c92911e2
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-11-17 02:20:43 +0800

    added checks out for visitor out caf

--- lib/screens/security personnel/security_dashboard.dart
+++ lib/screens/security personnel/security_dashboard.dart
@@ -1,7 +1,10 @@
-import 'package:communisyncmobile/constants/custom_clipper.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:communisyncmobile/constants/custom_clipper.dart';
 import '../../backend/api/personnel/checks_current_visitor.dart';
+import '../../backend/api/personnel/checks_out_visitor.dart';
+import '../../backend/model/models.dart';
 
 class SecurityDashboard extends StatefulWidget {
   const SecurityDashboard({Key? key}) : super(key: key);
@@ -9,11 +12,79 @@ class SecurityDashboard extends StatefulWidget {
   @override
   State<SecurityDashboard> createState() => _SecurityDashboardState();
 }
-final _formKey = GlobalKey<FormState>();
-final TextEditingController _searchController = TextEditingController();
+
 class _SecurityDashboardState extends State<SecurityDashboard> {
+  late Future<List<Logbook>> _logbookFuture;
+  bool _mounted = false; // Track the mounted state of the widget
+
+  @override
+  void initState() {
+    super.initState();
+    _mounted = true; // Set to true when the widget is mounted
+    _logbookFuture = _fetchLogbookData();
+  }
+
+  @override
+  void dispose() {
+    _mounted = false; // Set to false when the widget is disposed
+    super.dispose();
+  }
+
+  Future<void> _showDeleteConfirmationDialog(Logbook logbook, int id) async {
+    bool? confirmed = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm out'),
+          content: const Text('Are you sure you want to check out this visitor?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop(true);
+
+                try {
+                  await outVisitor(logbook.id); // Perform deletion
+                  if (_mounted) {
+                    // Refresh data after deletion
+                    _logbookFuture = _fetchLogbookData();
+                    setState(() {});
+                  }
+                } catch (e) {
+                  // Handle errors
+                  print('Error deleting item: $e');
+                }
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmed == true) {
+      print('Deleting item with id: $id');
+    }
+  }
+
+  Future<List<Logbook>> _fetchLogbookData() async {
+    try {
+      return await checksCurrentVisitors();
+    } catch (e) {
+      print('Error fetching logbook data: $e');
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    String host = dotenv.get("API_HOST", fallback: "");
+
     return Scaffold(
       body: NestedScrollView(
         floatHeaderSlivers: true,
@@ -31,8 +102,11 @@ class _SecurityDashboardState extends State<SecurityDashboard> {
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(color: Colors.green.shade700),
                 child: Center(
-                  child: Image.asset('assets/images/logo-white.png',
-                      width: 160, height: 160),
+                  child: Image.asset(
+                    'assets/images/logo-white.png',
+                    width: 160,
+                    height: 160,
+                  ),
                 ),
               ),
             ),
@@ -42,84 +116,162 @@ class _SecurityDashboardState extends State<SecurityDashboard> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              TextField(
-                key: _formKey,
-                controller: _searchController,
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  prefixIcon: IconButton(
-                      icon: const Icon(Icons.menu), onPressed: () {}),
-                  suffixIcon: IconButton(
-                      icon: const Icon(Icons.search),
-                      onPressed: () async {
-                        await checksCurrentVisitors();
-                        setState(() {
-                          // _homeownersFuture =
-                          //     getCafSearchRequestApi(_searchController.text);
-                        });
-                      }),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Current Visitors',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
                   ),
-                  contentPadding: const EdgeInsets.symmetric(vertical: 12.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Add Manually pressed');
+                    },
+                    child: Text('Add Manually'),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: FutureBuilder<List<Logbook>>(
+                  future: _logbookFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return RefreshIndicator(
+                        onRefresh: () async {
+                          await _fetchLogbookData();
+                          setState(() {
+                            _logbookFuture = _fetchLogbookData();
+                          });
+                        },
+                        child: ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            Logbook logbook = snapshot.data![index];
+                            return Card(
+                              margin: const EdgeInsets.all(10),
+                              elevation: 12,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                              color: Colors.green,
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 5.0,
+                                      vertical: 20.0,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(24),
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          Colors.green.shade800,
+                                          Colors.green.shade400
+                                        ],
+                                      ),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(left: 10),
+                                          child: CircleAvatar(
+                                            radius: 24,
+                                            backgroundImage: logbook.visitor != null
+                                                ? NetworkImage('${host ?? ''}/storage/${logbook.visitor!.photo}')
+                                                : null,
+                                            child: logbook.visitor == null
+                                                ? Icon(Icons.person, size: 24, color: Colors.white)
+                                                : null,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 10),
+                                        const SizedBox(width: 10),
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              '${logbook.visitor?.firstName} ${logbook.visitor?.lastName}',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            Text(
+                                              'Date: ${logbook.visitDate}',
+                                              style: TextStyle(color: Colors.white),
+                                            ),
+                                            Text(
+                                              'Time: ${logbook.visitTime}',
+                                              style: TextStyle(color: Colors.white),
+                                            ),
+                                            if (logbook.visitMembers != null)
+                                              Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    'Members:',
+                                                    style: TextStyle(color: Colors.white),
+                                                  ),
+                                                  Column(
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    children: logbook.visitMembers!.map((member) {
+                                                      return Padding(
+                                                        padding: const EdgeInsets.only(bottom: 2.0),
+                                                        child: Text(
+                                                          member,
+                                                          style: TextStyle(color: Colors.white),
+                                                        ),
+                                                      );
+                                                    }).toList(),
+                                                  ),
+                                                ],
+                                              ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 10,
+                                    right: 10,
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.outbond,
+                                        color: Colors.white,
+                                        size: 30.0,
+                                      ),
+                                      onPressed: () {
+                                        _showDeleteConfirmationDialog(logbook, logbook.id);
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  },
                 ),
               ),
-              const SizedBox(height: 16.0),
-              // FutureBuilder<>(
-              //   future: ,
-              //   builder: (context, snapshot) {
-              //     if (snapshot.connectionState == ConnectionState.waiting) {
-              //       // While data is loading, display a loading indicator
-              //       return const CircularProgressIndicator();
-              //     } else if (snapshot.hasError) {
-              //       // If there's an error, display an error message
-              //       return Text('Error: ${snapshot.error}');
-              //     } else if (snapshot.hasData) {
-              //       // If data is available, display it using a ListView
-              //       final data = snapshot.data;
-              //       return Expanded(
-              //         child: ListView.builder(
-              //           itemCount: data!.length,
-              //           itemBuilder: (context, index) {
-              //             final homeowner = data[index];
-              //             return ListTile(
-              //               leading: const Icon(
-              //                 Icons.face,
-              //                 size: 40,
-              //               ),
-              //               subtitle: Text(
-              //                 homeowner.userName,
-              //                 style: TextStyle(
-              //                   color: Colors.black54.withOpacity(.3),
-              //                 ),
-              //               ),
-              //               title: Text(
-              //                 '${homeowner.firstName} ${homeowner.lastName}',
-              //                 style: const TextStyle(
-              //                   color: Colors.black,
-              //                   fontSize: 20,
-              //                 ),
-              //               ),
-              //               onTap: () {
-              //                 Navigator.push(
-              //                     context,
-              //                     MaterialPageRoute(
-              //                         builder: (context) =>
-              //                             TapSpecificName(homeowner: homeowner)
-              //                     )
-              //                 );
-              //               },
-              //               // Add more fields as needed
-              //             );
-              //           },
-              //         ),
-              //       );
-              //     } else {
-              //       // If there's no data, display a message
-              //       return const Text('No data available');
-              //     }
-              //   },
-              // )
+
             ],
           ),
         ),
